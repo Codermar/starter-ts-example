b828e60df9f34184a2d44f2b737523a8433ff853
Jose Alfonso <jose.alfonso@apple.com>
2023-11-06 08:19:59 -0800

    doc: ts config notes

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,15 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    // environment: 'jsdom', // for vue tests
+    // type of tests can also be specifie by globs: i.e.
+    // environmentMatchGlobs: [
+    //   ['**/test/**/*.test.ts', 'jsdom']
+    // ],
     globals: true,
     root: __dirname,
-    setupFiles: ['./vitest.setup.ts'],
+    setupFiles: [
+      './vitest.setup.ts',
+    ],
   },
 })
